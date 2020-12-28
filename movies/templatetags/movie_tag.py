f2be44422f7724ce08666c5208dd965111d7a5c7
Your Name <you@example.com>
2020-12-28 18:07:10 +0300

    Add last_film count

--- movies/templatetags/movie_tag.py
+++ movies/templatetags/movie_tag.py
@@ -12,6 +12,6 @@ def get_categories():
 
 
 @register.inclusion_tag('movies/tags/last_movie.html')
-def get_last_movies():
-    movies = Movie.objects.order_by('id')[:5]
+def get_last_movies(count=5):
+    movies = Movie.objects.order_by('id')[:count]
     return {'last_movies': movies}
